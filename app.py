4bd934439f528ab77d6b3c1df8f6ce0620157c95
Carlos M .Arias <carias@redhat.com>
2023-08-03 13:27:57 +0200

    added exception handle

--- app.py
+++ app.py
@@ -44,9 +44,8 @@ def intercom_login():
         try:
             WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="identifierId"]'))).send_keys("carias" + "@redhat.com")
             WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//*[contains(text(), "Next")]'))).click()
-        except Exception as e:
-            WebDriverWait(driver, 3).until(EC.element_to_be_clickable(
-                (By.XPATH, '//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/div/ul/li[1]/div/div[1]/div/div[2]/div[2]'))).click()
+        except:
+            WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/div/ul/li[1]/div/div[1]/div/div[2]/div[2]'))).click()
 
         # RH SSO
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="username"]'))).send_keys("carias")
@@ -54,8 +53,8 @@ def intercom_login():
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submit"]'))).click()
         time.sleep(5)
     except Exception as e:
-        handle_exception(e)
         logging.error("An exception occurred while accepting during login")
+        handle_exception(e)
 
 
 def skype_login():
@@ -80,9 +79,9 @@ def skype_login():
         click_popups()
         driver.switch_to.window(driver.window_handles[0])
     except Exception as e:
-        handle_exception(e)
         logging.error("Skype login failed")
         driver.switch_to.window(driver.window_handles[0])
+        handle_exception(e)
 
 def click_popups():
     # Click on notification and  tutorial pop-ups
@@ -101,8 +100,7 @@ def skype_call():
         try:
             WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[1]/div[3]/div[3]/div/div/div/div[2]/div[2]/div/button'))).click()
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div/div/div/div/button'))).click()
-        except Exception as e:
-            handle_exception(e)
+        except:
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div/div/div/div/button'))).click()
             pass
         # Wait for the hang_up
@@ -111,10 +109,10 @@ def skype_call():
         driver.refresh()
         driver.switch_to.window(driver.window_handles[0])
     except Exception as e:
-        handle_exception(e)
         driver.refresh()
         logging.error("Call failed")
         driver.switch_to.window(driver.window_handles[0])
+        handle_exception(e)
 
 def get_customer_name():
     try:
@@ -137,8 +135,7 @@ def is_expert_chat():
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/div[2]/div/div/div/div[3]/div/div[2]/div[1]/div[2]/div[1]//*[contains(text(), "Expert") or contains(text(), "@T2")]'))).text
         logging.debug("Is an expert chat")
         return True
-    except Exception as e:
-        handle_exception(e)
+    except:
         logging.debug("NOT an expert chat")
         return False
 
