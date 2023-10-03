246c3415c9c1f6f783fb019e4cbf1f873e3010fd
Carlos M .Arias <carias@redhat.com>
2023-10-03 17:10:51 +0200

    interface changes broke the user select

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def skype_call():
         # Make the call
         logging.info("Calling")
         # Select user
-        WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div/div[1]/div/div/div[3]/div[3]/div/div/div[2]'))).click()
+        WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//div[@data-text-as-pseudo-element="Carlos Arias"]'))).click()
         # Click call button
         WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div/div/div[1]/div[1]/div[1]/div[2]/div/button'))).click()
         # Do it without video or audio
